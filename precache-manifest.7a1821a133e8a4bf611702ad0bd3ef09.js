b37db691be7b36f3056a4c12b2cb4ca70117d9eb
Yanis Urbis <yanisurbis@gmail.com>
2019-01-13 23:55:40 +0300

    Updates

---
+++ precache-manifest.7a1821a133e8a4bf611702ad0bd3ef09.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac8ea2135e1379656f02ca2eea26cc87",
+    "url": "/test-project/static/media/star.ac8ea213.svg"
+  },
+  {
+    "revision": "5437e5214c32dc0a8b1e3e3749edd831",
+    "url": "/test-project/static/media/search.5437e521.svg"
+  },
+  {
+    "revision": "cbeab2705cd1a8c5d774eac8ed9c9619",
+    "url": "/test-project/static/media/dollar-sign.cbeab270.svg"
+  },
+  {
+    "revision": "1d4ab0808ed85d8b7bf52b4a7acb9b11",
+    "url": "/test-project/static/media/compass.1d4ab080.svg"
+  },
+  {
+    "revision": "c39990ee32df20deed32",
+    "url": "/test-project/static/js/runtime~main.c39990ee.js"
+  },
+  {
+    "revision": "5758b4db8a9cd83d64f7",
+    "url": "/test-project/static/js/main.5758b4db.chunk.js"
+  },
+  {
+    "revision": "49976361de609f123415",
+    "url": "/test-project/static/js/1.49976361.chunk.js"
+  },
+  {
+    "revision": "5758b4db8a9cd83d64f7",
+    "url": "/test-project/static/css/main.28aa8db4.chunk.css"
+  },
+  {
+    "revision": "12add8312132e30fb2916ddfb460c95b",
+    "url": "/test-project/index.html"
+  }
+];
